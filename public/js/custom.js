f2c8397d133e7c0fb772af35a591742c345c2894
Andriel Nuernebrg <andrielfn@gmail.com>
2011-03-31 22:31:18 -0300

    refactoring

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,105 +1,231 @@
-$(document).ready(function(){	
-	$(document).ready(function(){
-		if (localStorage.getItem("blinds") != null) {
-			var blinds = localStorage.getItem("blinds").split(",");
-			$("#set-blinds :input[type=text]").each(function(el){
-				$(this).val(blinds[el]);
-			});
-		}else{
-			// Set blinds by default
-			var blinds = [2, 4, 10, 20, 50, 100, 200, 400, 800, 1000, 2000, 4000]
-			localStorage.setItem("blinds", blinds);
-		}
+var DEFAULT_BLINDS = [2, 4, 10, 20, 50, 100, 200, 400, 800, 1000, 2000, 4000];
+var DEFAULT_TIMER = 600;
+var DEFAULT_LEVEL = 1;
+var DEFAULT_TIMER_STATUS = "paused"
+
+$(document).ready(function(){
+	
+	// Set blinds by default
+	if(getBlinds() == null){
+		setBlinds(DEFAULT_BLINDS);
+		setBigBlind(DEFAULT_BLINDS[0]);
+	}
+
+	// Set timer by default
+	if(getTimer() == null)
+		setTimer(DEFAULT_TIMER);
+
+	if(getLevel() == null)
+		setLevel(DEFAULT_LEVEL);
 		
-		if (localStorage.getItem("countdown") != null) {
-			countdown = parseInt(localStorage.getItem("countdown"));
-			var time = [];
-			time[0] = Math.floor(countdown / 60);
-			time[1] = countdown % 60;
-			$("#set-countdown :input[type=text]").each(function(el){
-				$(this).val(time[el]);
-			});
-			// alert(parseInt(localStorage.getItem("countdown")));
-		}else{
-			// Set countdown time per level
-			var countdown = 600; // In seconds
-			localStorage.setItem("countdown", countdown);
-		}
-	});
-	
-	$('#timer').countdown({until: +parseInt(localStorage.getItem("countdown")), compact: true, format: "MS", onTick: redOn5Seconds, onExpiry: updateData});
-	
-	
-	// Set default values to inputs
-	$("#set-default-blinds").click(function(){ 
-		var blinds = [2, 4, 10, 20, 50, 100, 200, 400, 800, 1000, 2000, 4000]
-		$("#set-blinds :input[type=text]").each(function(el){
-			$(this).val(blinds[el]);
-		});	
-	});
-	
-	// Clear inputs
-	$("#clear-default-blinds").click(function(){ 
-		$("#set-blinds :input[type=text]").each(function(el){
-			$(this).val("");
-		});
-	});
-	
-	// Save blinds values on local storage
-	$("#set-blinds :input[type=submit]").click(function(){
-		var blinds = new Array();
-		var save = true;
-		$("#set-blinds :input[type=text]").each(function(el){
-			if ($(this).val() == "") {
-				alert("Level " + (parseInt(el)+1) + " is blank.");
-				save = false;
-				return false;
-			};
-			blinds.push(parseInt($(this).val()));
-		});
-		if(save){
-			localStorage.setItem("blinds", blinds);
-		}
+	if(getTimerStatus() == null)
+		setTimerStatus(DEFAULT_TIMER_STATUS);
+
+	// Print blind values on page
+	printBlinds();
+
+	$(".close-header").click(function(){
+		closeHeader();
 		return false;
 	});
 	
-	// Save blinds values on local storage
-	$("#set-countdown :input[type=submit]").click(function(){
-		var save = true;
-		var inputs = new Array();
-		var countdown = 0;
-		inputs = $("#set-countdown :input[type=text]");
-		if(inputs[0].value != ""){
-			countdown += parseInt(inputs[0].value) * 60;
-		}
-		if(inputs[1].value != ""){
-			countdown += parseInt(inputs[1].value);
-		}
-		if(save){
-			localStorage.setItem("countdown", countdown);
-		}
+	$("#set-blinds :input[type='submit']").click(function(){
+		setCustomBlinds();
+		notify("Blinds saved.", "success");
 		return false;
 	});
 	
-	// ################## TEMP ##################
-	$("#temp-test").click(function(){
+	$("#set-timer :input[type='submit']").click(function(){
+		setCustomTimer();
+		notify("Timer saved.", "success");
+		return false;
 	});
+
+	$('#timer').countdown({until: +getTimer(), onTick: everySecond, onExpiry: updateTimer});
+	$('#timer').countdown('pause');
+	
+	fillBlindFields();
+	fillTimerField();
 });
 
-function redOn5Seconds(periods){
-	if ($.countdown.periodsToSeconds(periods) == 5){
-		$(this).addClass('red');
+function setCustomTimer(){
+	setTimer($("#set-timer :input[type='text']").val() * 60);
+}
+
+function fillTimerField(){
+	var minutes = parseInt(getTimer()) / 60;
+	$("#set-timer :input[type='text']").val(minutes);
+}
+
+function setCustomBlinds(){
+	var blinds = [];
+	$("#set-blinds :input[type='text']").each(function(el) {
+		blinds.push(parseInt($(this).val()));
+	});
+	setBlinds(blinds);
+}
+
+function fillBlindFields(){
+	var blinds = getBlinds();
+	$("#set-blinds :input[type='text']").each(function(el) {
+		$(this).val(blinds[el]);
+	});
+}
+
+function getBlinds(){
+	if(localStorage.getItem("cfg_blinds") == null)
+	return setBlinds(DEFAULT_BLINDS);
+	else
+	return localStorage.getItem("cfg_blinds").split(",");
+}
+
+function setBlinds(blinds){
+	localStorage.setItem("cfg_blinds", blinds);
+	return blinds;
+}
+
+function getBigBlind(){
+	if(localStorage.getItem("big_blind") == null)
+	return setBigBlind(DEFAULT_BLINDS[0]);
+	else
+	return localStorage.getItem("big_blind");
+}
+
+function setBigBlind(blind){
+	// alert(blind);
+	localStorage.setItem("big_blind", blind);
+	return blind;
+}
+
+function getSmallBlind(){
+	return getBigBlind() / 2;
+}
+
+function getTimer(){
+	if(localStorage.getItem("cfg_timer") == null)
+	return setTimer(DEFAULT_TIMER);
+	else{
+		return parseInt(localStorage.getItem("cfg_timer"));
 	}
 }
 
-function updateData(){
-	var buzzer = new Audio("/boom.mp3");
-	buzzer.play();
-	var blinds = localStorage.getItem("blinds").split(",");
-	var newBlindLevel = parseInt($(".level-number").html());
-	var bigBlind = blinds[newBlindLevel];
-	$(".small").html(bigBlind/2);
-	$(".big").html(bigBlind);
-	$(".level-number").html(parseInt($(".level-number").html()) + 1);
-	$('#timer').removeClass('red').countdown('change', {until: +parseInt(localStorage.getItem("countdown"))});
+function setTimer(seconds){
+	localStorage.setItem("cfg_timer", seconds);
+	return seconds;
+}
+
+function setLevel(level){
+	localStorage.setItem("current_level", level);
+	return level;
+}
+
+function getLevel(){
+	if(localStorage.getItem("current_level") == null){
+		return setLevel(DEFAULT_LEVEL);
+	}else{
+		return parseInt(localStorage.getItem("current_level"));
+	}
+}
+
+function setTimerStatus(status){
+	localStorage.setItem("timer_status", status);
+	return status;
+}
+
+function getTimerStatus(){
+	if(localStorage.getItem("timer_status") == null){
+		return setTimerStatus(DEFAULT_TIMER_STATUS);
+	}else{
+		return localStorage.getItem("timer_status");
+	}
+}
+
+function everySecond(periods){
+	if ($.countdown.periodsToSeconds(periods) == 5) { 
+		$(this).addClass('red'); 
+	}
+	
+	if (audio == null) {
+		var audio = new Audio("/5seconds.mp3");
+	}
+	
+	second = $.countdown.periodsToSeconds(periods)
+	if (second <= 5 && second >= 2) {
+		audio.play();
+	}
+}
+
+function updateTimer(){
+	setBigBlind(getBlinds()[getLevel()]);
+	setLevel(getLevel() + 1);
+	printBlinds();
+	printLevel();
+	$('#timer').removeClass('red').countdown('change', {until: +getTimer()});
+}
+
+function printBlinds(){
+	var bigBlind = getBigBlind();
+	var smallBlind = getSmallBlind();
+	$("#blinds .small").html(smallBlind);
+	$("#blinds .big").html(bigBlind);
+}
+
+function printLevel(){
+	$(".level-number").html(getLevel());
+}
+
+function notify(message, class_name){
+	$.notifyBar({
+		html: message,
+		delay: 2000,
+		animationSpeed: "normal",
+		cls: class_name
+	});
+}
+
+function closeHeader(){
+	$("header").slideToggle('medium');
+	$(this).html("+");
+}
+
+// ################## KEYS ##################
+$(window).keypress(function(e){
+	switch(e.keyCode){
+		case 32:
+			pauseOrResumeTimer();
+			return false;
+		case 114:
+			resetAll();
+			return false;
+		default:
+			return true;
+	}
+});
+
+function resetAll(){
+	var sure = confirm("Are you sure?");
+	if (!sure) { return false };
+	$("#timer").countdown('change', {until: +getTimer()});
+	$('#timer').countdown('pause');
+	notify('Timer reseted. Press Space to start again.', "success")
+	setBlinds(DEFAULT_BLINDS);
+	setBigBlind(DEFAULT_BLINDS[0]);
+	setTimer(DEFAULT_TIMER);
+	setLevel(DEFAULT_LEVEL);
+	setTimerStatus(DEFAULT_TIMER_STATUS);
+	printBlinds();
+	printLevel();
+}
+
+function pauseOrResumeTimer(){
+	// closeHeader();
+	if(getTimerStatus() == 'paused'){
+		$('#timer').countdown('resume');
+		setTimerStatus('running');
+		notify('Timer started.', "success");
+	}else{
+		$('#timer').countdown('pause');
+		setTimerStatus('paused');
+		notify('Timer paused.', "success");
+	}
 }
